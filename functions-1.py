34ef3294b298d83456268efd56b380eb099abe31
mohammed98k <sun.mohamed17@gmail.com>
2023-08-05 11:19:10 +0300

    first commit

---
+++ functions-1.py
@@ -0,0 +1,35 @@
+# n1 = int(input("n1: "))
+# n2 = int(input("n2: "))
+# def sum(n1,n2):
+#     total = n1+n2
+#     print ("sum:",total)
+#
+# sum(n1,n2)
+#
+# def subtraction(n1,n2):
+#     sub = n1-n2
+#     print ("sub:",sub)
+#
+# subtraction(n1,n2)
+#
+# def division(n1,n2):
+#     total = n1/n2
+#     print ("total:",total)
+#
+# division(n1,n2)
+#
+#
+# def multiply(n1,n2):
+#     total = n1*n2
+#     print ("total:",total)
+#
+# multiply(n1,n2)
+
+
+
+num =int(input("num:"))
+def test():
+    cu = num*num*num
+    print(cu)
+
+test()
