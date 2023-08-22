51eb094c3380fb96a2441217b91d41f1f96e75ef
mohammed98k <sun.mohamed17@gmail.com>
2023-08-22 12:05:17 +0300

    my commit

---
+++ files-1.py
@@ -0,0 +1,10 @@
+with open("details-1.txt","w") as file:
+    students = []
+    name = input("student name: ")
+    age = int(input("student age: "))
+    dob = int(input("student dob: "))
+    informations = {"name": name , "age" : age , "dob" : dob}
+    file.write(f"Name: {name}\nAge: {age}\nDOB: {dob}\n")
+    students.append(informations)
+    file.close()
+    print(students)
