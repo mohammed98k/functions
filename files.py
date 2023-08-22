b3d5fe04432591e0cd1670dfaee7db0e8756c2be
mohammed98k <76209805+mohammed98k@users.noreply.github.com>
2023-08-22 11:55:46 +0300

    Delete files.py

--- files.py
+++
@@ -1,9 +0,0 @@
-with open("details.txt","w") as file:
-    students = []
-    name = input("student name: ")
-    age = int(input("student age: "))
-    dob = int(input("student dob: "))
-    informations = {"name": name , "age" : age , "dob" : dob}
-    students.append(informations)
-    file.close()
-    print(students)
